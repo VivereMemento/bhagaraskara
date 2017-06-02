7d160c6d560c18a4044c9b6c7a9eb5eb264f64a8
Alexandr Kuznetsov <memento_viver@ukr.net>
2017-06-02 09:25:43 +0300

    header animation

--- source/js/app.js
+++ source/js/app.js
@@ -3,17 +3,35 @@
 
     svg4everybody();
 
-// ***** navigation ***** //
-
-const trig = document.querySelector('.nav__trigger');
-trig.addEventListener('click', function() {
-    let navList = document.querySelector('.nav__list');
+    // ***** navigation ***** //
     
-    if(navList.classList.contains('opened')) {
-        navList.classList.remove('opened');
-    } else {
-        navList.classList.add('opened');
-    }
-});
+    const tl = new TimelineMax(),
+          navItems = document.querySelectorAll('.header__nav-link'),
+          svgElements = document.querySelectorAll('.header__svg-elem'),
+          trig = document.querySelector('.header__link-down'),
+          heading = document.querySelector('.heading__text'),
+          subHeading = document.querySelector('.subheading__text'),
+          logo = document.querySelector('.logo');
+
+    
+    window.addEventListener('load', function() {
+        TweenMax.set('.logo', {x:"-1000px"});
+        TweenMax.set([heading, subHeading], {opacity: 0, y:"-30px"});
+        TweenMax.set(navItems, {opacity: 0});
+        TweenMax.set(trig, {opacity: 0, y: "100px"});
+        tl.to(logo, 3, {x: 0, ease:Elastic.easeOut})
+            .to(heading, 1, {opacity: 1, y: 0, ease:Linear.easeNone})
+            .to(subHeading, 1, {opacity: 1, y: 0, ease:Linear.easeNone, onComplete: animatedLogo});
+    });
+
+    function animatedLogo () {
+        const items = document.querySelectorAll('.header__svg');
+        items.forEach(item => {
+            item.classList.add('action');
+        });
+        tl.to(navItems, 0.5, {opacity: 1})
+        .staggerFromTo(svgElements, 2, {opacity: 0, scale: 0}, {opacity: 1, scale: 1, ease:Elastic.easeOut}, 0.5)
+        .to(trig, 0.5, {opacity: 1, x: 0, y: 0, ease:Bounce.easeOut}, "-=1.5");
+    };
 
 })();
